875f7052b825c59314e741d42664f861b516f814
s25067 <s25067@pjatk.edu.pl>
2023-06-10 15:25:48 +0200

    update

--- src/main/java/com/example/movieservice/model/Movie.java
+++ src/main/java/com/example/movieservice/model/Movie.java
@@ -1,17 +1,25 @@
 package com.example.movieservice.model;
 
 import com.example.movieservice.model.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Schema(description = "id filmu")
     private Long id;
+    @Schema(description = "tytuł filmu")
+    @Size(min = 1, max = 50)
     private String name;
+    @Schema(description = "kategoria filmu")
     @Enumerated(EnumType.STRING)
     private Category category;
+    @Schema(description = "dostępność filmu")
     private boolean isAvailable;
     private boolean active;
 
